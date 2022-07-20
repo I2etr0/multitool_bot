49c97c29a795eb3bc9a781f6f7ddf087e15f83a7
I2etr0 <47482322+I2etr0@users.noreply.github.com>
2022-07-20 03:09:40 +0300

    Add new file - weather_parser.py
    A lot of work has been done - the "Weather" button has been added. When pressed, three more buttons appear:
    "Today", "Tomorrow" and "The Day After Tomorrow". When you press the button, data on the day drops out (day of the week, date, month and weather data day and night)
    
    
    Along the way, I learned to write classes in Python and applied these skills in practice.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import parser
+import currency_parser
+import weather_parser
 import telebot
 from telebot import types
 
@@ -10,7 +11,7 @@ bot = telebot.TeleBot(TOKEN)
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     currency = types.KeyboardButton('Курс валют')
-    link = types.KeyboardButton('Сократить ссылку')
+    link = types.KeyboardButton('Погода')
     markup.add(currency, link)
 
     bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user), reply_markup=markup)
@@ -29,17 +30,32 @@ def menu(message):
 
         bot.send_message(message.chat.id, 'Выбери валюту:'.format(message.from_user), reply_markup=markup)
 
-    elif message.text == 'Сократить ссылку':
-        pass
+    elif message.text == 'Погода':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        today = types.KeyboardButton('Сегодня')
+        tomorrow = types.KeyboardButton('Завтра')
+        tomorrow_x2 = types.KeyboardButton('Послезавтра')
+        markup.add(today, tomorrow, tomorrow_x2)
+
+        bot.send_message(message.chat.id, 'Выбери день:'.format(message.from_user), reply_markup=markup)
+
+    elif message.text == 'Сегодня':
+        bot.send_message(message.chat.id, f"{message.text}:\n" + str(weather_parser.Today.other.r[0][0:80]))
+
+    elif message.text == 'Завтра':
+        bot.send_message(message.chat.id, f"{message.text}:\n" + str(weather_parser.Other_days.other.r[1][0:80]))
+
+    elif message.text == 'Послезавтра':
+        bot.send_message(message.chat.id, f"{message.text}:\n" + str(weather_parser.Other_days.other.r[2][0:80]))
 
     elif message.text == 'Доллар':
-        bot.send_message(message.chat.id, f"{message.text}: " + str(parser.r[0][-7:] + f' на {parser.date[-13:]}'))
+        bot.send_message(message.chat.id, f"{message.text}: " + str(currency_parser.r[0][-7:] + f' на {currency_parser.date[-13:]}'))
 
     elif message.text == 'Евро':
-        bot.send_message(message.chat.id, f"{message.text}: " + str(parser.r[1][-7:] + f' на {parser.date[-13:]}'))
+        bot.send_message(message.chat.id, f"{message.text}: " + str(currency_parser.r[1][-7:] + f' на {currency_parser.date[-13:]}'))
 
     elif message.text == 'Грузинский Лари':
-        bot.send_message(message.chat.id, f"{message.text}: " + str(parser.r[66][-7:] + f' на {parser.date[-13:]}'))
+        bot.send_message(message.chat.id, f"{message.text}: " + str(currency_parser.r[66][-7:] + f' на {currency_parser.date[-13:]}'))
 
 
 bot.polling(none_stop=True)
