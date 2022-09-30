3a1674e8c0da2c706332876fde4ebed2fa92defd
Yaroslav <y.gorodilov@ibc.ru>
2022-09-30 13:53:36 +0500

    Подготовил шаблон для pythonanywhere
    START_POINT

--- bot.py
+++ bot.py
@@ -3,51 +3,40 @@ import currency_parser
 import telebot
 from telebot import types
 
-TOKEN = token.token
+TOKEN = '5417008537:AAFJinudA_4-BrwFv0xaVlpXWXPS6DDBUYI'
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands='start')
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    currency = types.KeyboardButton('Курс валют')
-    link = types.KeyboardButton('Погода')
-    markup.add(currency, link)
+    usd = types.KeyboardButton('Доллар')
+    eur = types.KeyboardButton('Евро')
+    gel = types.KeyboardButton('Грузинский Лари')
+    markup.add(usd, eur, gel)
 
     bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user), reply_markup=markup)
 
+    # elif message.text == 'Доллар':
+    #     bot.send_message(message.chat.id,
+    #                      f"{message.text}: " + str(currency_parser.r[0][-7:] + f' на {currency_parser.date[-13:]}'))
+    #
+    # elif message.text == 'Евро':
+    #     bot.send_message(message.chat.id,
+    #                      f"{message.text}: " + str(currency_parser.r[1][-7:] + f' на {currency_parser.date[-13:]}'))
+    #
+    # elif message.text == 'Грузинский Лари':
+    #     bot.send_message(message.chat.id,
+    #                      f"{message.text}: " + str(currency_parser.r[66][-7:] + f' на {currency_parser.date[-13:]}'))
 
-@bot.message_handler(content_types='text')
-def menu(message):
-    if message.text == 'Курс валют':
-
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        usd = types.KeyboardButton('Доллар')
-        eur = types.KeyboardButton('Евро')
-        gel = types.KeyboardButton('Грузинский Лари')
-        back = types.KeyboardButton('Назад')
-        markup.add(usd, eur, gel, back)
-
-        bot.send_message(message.chat.id, 'Выбери валюту:'.format(message.from_user), reply_markup=markup)
-
-    elif message.text == 'Доллар':
-        bot.send_message(message.chat.id, f"{message.text}: " + str(currency_parser.r[0][-7:] + f' на {currency_parser.date[-13:]}'))
+    if message.text == 'Доллар':
+        bot.send_message(message.chat.id, f"{message.text}: ")
 
     elif message.text == 'Евро':
-        bot.send_message(message.chat.id, f"{message.text}: " + str(currency_parser.r[1][-7:] + f' на {currency_parser.date[-13:]}'))
+        bot.send_message(message.chat.id, f"{message.text}: ")
 
     elif message.text == 'Грузинский Лари':
-        bot.send_message(message.chat.id, f"{message.text}: " + str(currency_parser.r[66][-7:] + f' на {currency_parser.date[-13:]}'))
-
-    elif message.text == 'Назад':
-
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        currency = types.KeyboardButton('Курс валют')
-        link = types.KeyboardButton('Погода')
-        markup.add(currency, link)
-
-        bot.send_message(message.chat.id, 'Чем могу помочь?'.format(message.from_user), reply_markup=markup)
-
+        bot.send_message(message.chat.id, f"{message.text}: ")
 
 
 bot.polling(none_stop=True)
